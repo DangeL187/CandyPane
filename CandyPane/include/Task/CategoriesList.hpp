ea1393308873117004c067c7b2e5481c88c8951e
DangeL <92173667+DangeL187@users.noreply.github.com>
2024-03-19 11:15:40 +0300

    Документация некоторых полей и методов в CategoriesList.hpp

--- CandyPane/include/Task/CategoriesList.hpp
+++ CandyPane/include/Task/CategoriesList.hpp
@@ -6,6 +6,27 @@
 /*! @class candypane::CategoriesList
     @brief Container for Category objects. Stores pointer to the main category.
 */
+/*! @fn void candypane::CategoriesList(Category* main_category)
+    @brief Adds new Task
+    @param main_category Pointer to the main Category object
+*/
+/*! @fn void candypane::CategoriesList::addCategory(const Category& new_category)
+    @brief Adds new Category
+    @param new_category Category object to add
+*/
+/*! @fn std::vector<Category> candypane::CategoriesList::getCategories()
+    @brief Get list of the Category objects
+    @return Vector of Category objects
+*/
+/*! @fn Category& candypane::CategoriesList::getCategoryById(unsigned long long int id)
+    @brief Get Category object by it's index in the list of the Category objects
+    @param id Category's index in the list of the Category objects
+    @return Category object
+*/
+//...
+/*! @var candypane::_categories
+    @brief Stores Category objects
+*/
 /*! @var candypane::_main_category
     @brief Used for updating main category
 */
