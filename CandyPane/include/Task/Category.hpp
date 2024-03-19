be7aae2f6196638f477a4ad2c025bb6e9c2c3ed8
DangeL <92173667+DangeL187@users.noreply.github.com>
2024-03-19 11:31:56 +0300

    Update Category.hpp

--- CandyPane/include/Task/Category.hpp
+++ CandyPane/include/Task/Category.hpp
@@ -7,29 +7,41 @@
 /*! @class candypane::Category
     @brief Class used to store and manage Tasks, stores a pointer to the parent categories list
 */
-/*! @fn candypane::Category()
-    @brief Default constructor 
+/*! @fn candypane::Category(CategoriesList* categories_list = nullptr)
+    @brief Creates Category object and sets pointer to the parent categories list
 */
 /*! @fn void candypane::Category::addTask(const Task& new_task)
     @brief Adds new Task
-    @param new_task new Task object
+    @param new_task New Task object
 */
 /*! @fn std::vector<Task> candypane::Category::getTasks()
     @brief Return the vector with all Tasks
     @return Vector of Tasks
 */
+/*! @fn std::string candypane::Category::getName()
+    @brief Get the Category's name
+    @return Category's name
+*/
 /*! @fn Task& candypane::Category::getTaskById(int id)
     @brief Return specific Task by it's index(id)
-    @param id index of the Task to get
+    @param id Index of the Task to get
     @return Task object
 */
 /*! @fn void candypane::Category::removeTask(const Task& task_to_remove)
     @brief Remove all Tasks that match the passed Task object.
     @param task_to_remove Task object to remove
 */
-/*! @fn void candypane::Category::removeTaskById(int id);
+/*! @fn void candypane::Category::removeTaskById(int id)
     @brief Removes Task by it's index(id)
-    @param id index of the Task to remove
+    @param id Index of the Task to remove
+*/
+/*! @fn void candypane::Category::setName(const std::string& new_name)
+    @brief Set new Category's name
+    @param new_name New Category's name
+*/
+/*! @fn void candypane::Category::setTasks(const std::vector<Task>& new_tasks)
+    @brief Set new tasks list
+    @param new_tasks Vector of Task objects
 */
 
 namespace candypane {
