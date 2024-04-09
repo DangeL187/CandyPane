721d5130cd6b8cdc42ee8fccb334b98a1a0c3785
DangeL <92173667+DangeL187@users.noreply.github.com>
2024-04-09 15:23:23 +0300

    Add files via upload

--- CandyPane/include/Task/CategoryList.hpp
+++ CandyPane/include/Task/CategoryList.hpp
@@ -1,7 +1,7 @@
 #ifndef CANDYPANE_CATEGORYLIST_HPP
 #define CANDYPANE_CATEGORYLIST_HPP
 
-#include "Task/Category.hpp"
+#include "Category.hpp"
 
 /*! @class candypane::CategoryList
     @brief Container for Category objects
@@ -15,18 +15,21 @@ namespace candypane {
 
         void addCategory(const Category& new_category);
         [[nodiscard]] std::vector<Category> getCategories() const;
-        Category& getCategoryById(unsigned long long int id);
+        Category& getCategoryById(long long id);
         Category& getCategoryByName(const std::string& name);
         unsigned long long getCategoriesSize();
-        void insertCategory(unsigned long long index, const Category& category);
+        [[nodiscard]] long long getHoveredId() const;
+        void insertCategory(long long index, const Category& category);
         void loadCategoryList();
-        void relocateCategoryById(unsigned long long int index, unsigned long long int category_id);
+        void relocateCategoryById(long long index, long long category_id);
         void removeCategory(const Category& category_to_remove);
-        void removeCategoryById(unsigned long long int id);
-        void swapCategoriesById(unsigned long long int id1, unsigned long long int id2);
+        void removeCategoryById(long long category_id);
+        void setHoveredId(long long category_id);
+        void swapCategoriesById(unsigned long long id1, unsigned long long id2);
 
     protected:
         std::vector<Category>   _categories;
+        long long               _hovered_id = -1;
     };
 
 }
--- CandyPane/include/Task/Task.hpp
+++ CandyPane/include/Task/Task.hpp
@@ -2,6 +2,7 @@
 #define CANDYPANE_TASK_HPP
 
 #include <string>
+#include <DateTime/DateTimeModule.hpp>
 
 /*! @class candypane::Task
     @brief Class that represents one specific task
@@ -13,43 +14,43 @@
     @brief Return name of the Task
     @return Name of the task
 */
-/*! @fn std::string candypane::Task::getText()
-    @brief Return text of the Task
-    @return Text of the task
-*/
 /*! @fn void candypane::Task::setName(const std::string& new_name)
     @brief Set name of the Task
     @param new_name new name of the Task
 */
-/*! @fn void candypane::Task::setText(const std::string& text)
-    @brief Set text of the Task
-    @param text text of the Task to set
-*/
 
 namespace candypane {
 
+    class Category; // forward declaration
+
     class Task {
     public:
-        Task() = default;
+        Task(Category* category);
 
+        [[nodiscard]] std::string getCategoryName() const;
+        [[nodiscard]] Date getDueDate() const;
         [[nodiscard]] std::string getName() const;
-        [[nodiscard]] std::string getText() const;
+        [[nodiscard]] Date getReminderDate() const;
+        [[nodiscard]] Time getReminderTime() const;
         [[nodiscard]] bool isCompleted() const;
         [[nodiscard]] bool isFavourite() const;
         void setCompleted(bool value);
+        void setDueDate(const Date& due_date);
         void setFavourite(bool value);
         void setName(const std::string &new_name);
-        void setText(const std::string &text);
+        void setReminderDate(const Date& reminder_date);
+        void setReminderTime(const Time& reminder_time);
 
-        bool operator==(const Task &other) const {
-            return (_name == other.getName() && _text == other.getText());
-        }
+        bool operator==(const Task &other) const;
 
     protected:
+        Category*   _category;
         bool        _completed = false;
+        Date        _due_date;
         bool        _favourite = false;
         std::string _name;
-        std::string _text; // TODO: delete?
+        Date        _reminder_date;
+        Time        _reminder_time;
     };
 
 }
